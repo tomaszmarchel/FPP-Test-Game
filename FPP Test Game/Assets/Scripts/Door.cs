1e7e18e28fee5c2178af1e6e25fcc28a28ec3582
Tomek <tomek.marchel@o2.pl>
2024-06-27 13:50:15 +0200

    Door prefab + adjusting device visual + rander texture

---
+++ FPP Test Game/Assets/Scripts/Door.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    [SerializeField] int doorValue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        doorValue = Random.Range(1, 9);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
